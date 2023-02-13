28b69e25d6d13118fa6dad26646a4e6d323b41dd
koteshwar <mkraofx@gmail.com>
2023-02-14 00:15:43 +0530

    changes

--- app/user/views.py
+++ app/user/views.py
@@ -6,6 +6,6 @@ from rest_framework import generics
 
 from user.serializers import UserSerializer
 
-class CreateUserView(generics:createAPIView):
-    """Create a new user in a system"""
+class CreateUserView(generics:CreateAPIView):
+    """Create a new user in the system"""
     serializer_class = UserSerializer
