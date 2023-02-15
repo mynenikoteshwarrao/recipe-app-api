4a80d4d3ce0162b5f3e0d05aa1cf8a664ed445a6
koteshwar <mkraofx@gmail.com>
2023-02-15 16:16:48 +0530

    rr

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,13 +1,10 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import (
-    get_user_model,
-    authenticate,
-)
-from django.utils.translation import gettext as _
+from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
@@ -20,15 +17,3 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
-
-    def update(self, instance, validated_data):
-        """Update and return user."""
-        password = validated_data.pop('password', None)
-        user = super().update(instance, validated_data)
-
-        if password:
-            user.set_password(password)
-            user.save()
-
-        return user
-
